fa7e23c6a895cb9c180836a637dd5e2a0e7a54c6
Sakshipunjabii <punjabisakshi10@gmail.com>
2024-09-17 18:51:31 +0530

    update

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+
+const port=8000;
+
+let task_obj=[];
+
+app.get('/todolist',(req,res)=>{
+        res.render('views',{task_obj});
+})
+    
+    
+app.post('/todolist',(req,res)=>{
+        console.log(req.body);
+        const task=req.body.task;
+        task_obj.push(task);
+        console.log(task_obj);
+        res.redirect('/todolist');
+
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening at port ${port}`);
+})
